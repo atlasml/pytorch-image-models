57e0a47741a75b4ac84f6d550ea250b9e396206b
Ross Taylor <rj-taylor@live.co.uk>
2019-06-17 18:05:37 +0100

    Update benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -11,7 +11,7 @@ def benchmark():
 
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     input_transform = transforms.Compose([
-        transforms.Resize(256, 'PIL.Image.BICUBIC'),
+        transforms.Resize(256, PIL.Image.BICUBIC),
         transforms.CenterCrop(224),
         transforms.ToTensor(),
         normalize,
