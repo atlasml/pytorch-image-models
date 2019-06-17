514c9a8d61d4fc0276362b1cb6cb4a8fe599fc8c
Ross Taylor <rj-taylor@live.co.uk>
2019-06-17 16:40:53 +0100

    Update benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -15,49 +15,10 @@ def benchmark():
 
     imagenet.benchmark(
         model=tf_efficientnet_b0(num_classes=1000, pretrained=True, input_transform=input_transform),
-        paper_model_name='EfficientNet B0',
+        paper_model_name='EfficientNet',
         paper_arxiv_id='1905.11946',
-        paper_pwc_id='efficientnet-rethinking-model-scaling-for'
-    )
-    
-    input_transform = transforms.Compose([
-        transforms.Resize(272, 'PIL.Image.BICUBIC'),
-        transforms.CenterCrop(240),
-        transforms.ToTensor(),
-        normalize,
-    ])
-    
-    imagenet.benchmark(
-        model=tf_efficientnet_b1(num_classes=1000, pretrained=True, input_transform=input_transform),
-        paper_model_name='EfficientNet B1',
-        paper_arxiv_id='1905.11946',
-        paper_pwc_id='efficientnet-rethinking-model-scaling-for'
-    )
-    
-    input_transform = transforms.Compose([
-        transforms.Resize(292, 'PIL.Image.BICUBIC'),
-        transforms.CenterCrop(260),
-        transforms.ToTensor(),
-        normalize,
-    ])
-    
-    imagenet.benchmark(
-        model=tf_efficientnet_b2(num_classes=1000, pretrained=True, input_transform=input_transform),
-        paper_model_name='EfficientNet B2',
-        paper_arxiv_id='1905.11946',
-        paper_pwc_id='efficientnet-rethinking-model-scaling-for'
-    )
-    
-    input_transform = transforms.Compose([
-        transforms.Resize(331, 'PIL.Image.BICUBIC'),
-        transforms.CenterCrop(300),
-        transforms.ToTensor(),
-        normalize,
-    ])
-    
-    imagenet.benchmark(
-        model=tf_efficientnet_b3(num_classes=1000, pretrained=True, input_transform=input_transform),
-        paper_model_name='EfficientNet B3',
-        paper_arxiv_id='1905.11946',
-        paper_pwc_id='efficientnet-rethinking-model-scaling-for'
+        paper_pwc_id='efficientnet-rethinking-model-scaling-for',
+        input_transform=input_transform,
+        batch_size=256,
+        num_gpu=1
     )
